c627b054770acfb1f3892d1b4bf62f316072ca13
tetheus4546b <vinicius.souza.matheus@escola.pr.gov.br>
2025-08-17 21:44:01 -0300

    vendo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', revelar-palavrachave);
 
 function revelar-palavrachave() {
-    const texto = document.querySelector('#Início-do-texto').value;
+    const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processamento-dos-textos(texto);
 
