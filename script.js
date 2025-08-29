9184e6a4c2c6ec6f877dac1e0ac2a5161061bea0
tetheus4546b <vinicius.souza.matheus@escola.pr.gov.br>
2025-08-29 19:25:43 -0300

    mudando

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const escrita = document.querySelector('#entrada-de-texto').value;
+    const texto = document.querySelector('#entrada-de-texto').value;
     const areaResultado = document.querySelector('#resultado-palavrachave');
-    const palavrasCerta =  RecebendoTexto(escrita);
+    const palavrasCerta =  RecebendoTexto(texto);
 
     areaResultado.textContent = palavrasCerta.join(", ");
 }
 
-function RecebendoTexto(escrita) {
+function RecebendoTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
     return palavras;
 }
